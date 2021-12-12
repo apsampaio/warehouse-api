28e979ac5b5b5c853f5e84516a570d00a797b579
apsampaio <andre03.sampaio@gmail.com>
2021-12-12 12:49:07 -0300

    add Item entity

---
+++ Entities/Item.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Warehouse.Entities
+{
+    public record Item
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+        public int Quantity { get; init; }
+        public string Type { get; init; }
+        public string Image { get; init; }
+        public DateTimeOffset CreatedAt { get; init; }
+        public DateTimeOffset UpdatedAt { get; init; }
+    }
+}
